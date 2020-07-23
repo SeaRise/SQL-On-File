bc42f8ebe8978c399bb0790d72b85eae53c49f78
SeaRise <846579815@qq.com>
2020-07-23 22:31:40 +0800

    remove todo

--- src/main/java/com/searise/sof/type/DataType.java
+++ src/main/java/com/searise/sof/type/DataType.java
@@ -3,9 +3,6 @@ package com.searise.sof.type;
 import com.searise.sof.core.SofException;
 import org.apache.commons.lang3.StringUtils;
 
-// todo 支持dataType convert
-//  现在是强类型限制,在analyse那里加上一个cast算子,
-//  用于转换类型
 public enum DataType {
     BooleanType(0, "bool", "Boolean"),
     StringType(1, "string", "String"),
