0e38070c2639ad040cbc30a37b1b53f08e57c01b
SeaRise <846579815@qq.com>
2021-01-19 23:24:10 +0800

    u

--- src/test/java/com/searise/sof/storge/memory/MemoryAllocatorSuite.java
+++ src/test/java/com/searise/sof/storge/memory/MemoryAllocatorSuite.java
@@ -1,4 +1,32 @@
 package com.searise.sof.storge.memory;
 
+import com.google.common.base.Preconditions;
+import org.junit.Test;
+
+import java.nio.ByteBuffer;
+
 public class MemoryAllocatorSuite {
+    @Test
+    public void test() {
+        MemoryAllocator allocator = new MemoryAllocator();
+        MemoryBlock block = allocator.allocate(1);
+        ByteBuffer buffer = block.byteBuffer;
+        allocator.free(block);
+        Preconditions.checkArgument(block.byteBuffer == null);
+        block = allocator.allocate(1);
+        // 小block不会缓存复用.
+        Preconditions.checkArgument(block.byteBuffer != buffer);
+        allocator.free(block);
+        Preconditions.checkArgument(block.byteBuffer == null);
+
+        block = allocator.allocate(5 * 1024 * 1024);
+        buffer = block.byteBuffer;
+        allocator.free(block);
+        Preconditions.checkArgument(block.byteBuffer == null);
+        block = allocator.allocate(5 * 1024 * 1024);
+        // 大block缓存复用
+        Preconditions.checkArgument(block.byteBuffer == buffer);
+        allocator.free(block);
+        Preconditions.checkArgument(block.byteBuffer == null);
+    }
 }
